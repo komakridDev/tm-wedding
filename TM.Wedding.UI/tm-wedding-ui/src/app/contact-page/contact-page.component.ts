f419a5d45706db2836485cf7ff9d5546ede2df63
komakrid <komakrid@gmail.com>
2023-09-25 21:44:09 +0300

    implement contact us page

--- TM.Wedding.UI/tm-wedding-ui/src/app/contact-page/contact-page.component.ts
+++ TM.Wedding.UI/tm-wedding-ui/src/app/contact-page/contact-page.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import emailjs from '@emailjs/browser';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
-export class ContactPageComponent {
+export class ContactPageComponent implements OnInit, OnDestroy {
+  rsvpForm: FormGroup;
+  submitterName: FormControl;
+  submitterSurname: FormControl;
+  email: FormControl;
+  phone: FormControl;
+  comments: FormControl;
+  isLoading: boolean = false;
+  errorMessage: string = "";
 
+  constructor(private router: Router, private spinner: NgxSpinnerService) {
+    this.isLoading = false;
+    this.submitterName = new FormControl('', [Validators.required]);
+    this.submitterSurname = new FormControl('', [Validators.required]);
+    this.email = new FormControl('');
+    this.phone = new FormControl('', [Validators.required]);
+    this.comments = new FormControl('');
+
+    this.rsvpForm = new FormGroup({
+      submitterName: this.submitterName,
+      submitterSurname: this.submitterSurname,
+      email: this.email,
+      phone: this.phone,
+      comments: this.comments
+    });
+  }
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.spinner.show();
+    setTimeout(() => {
+      this.spinner.hide();
+      this.isLoading = false;
+    }, 2000);
+  }
+  ngOnDestroy(): void {
+    this.isLoading = false;
+  }
+
+  onSubmit() {
+    this.isLoading = true;
+    this.spinner.show();
+
+    try{
+      setTimeout(() => {
+        this.sendContactResponse();
+        this.rsvpForm.reset();
+        this.router.navigateByUrl("/thankyou");
+        this.spinner.hide();
+      }, 4000);
+
+    }
+    catch(err){
+      this.isLoading = false;
+      this.spinner.hide();
+      this.errorMessage = "There was an error during submission please try again!"
+    }
+
+  }
+
+  private sendContactResponse(){
+
+    console.log("sending Contact form via email");
+
+    emailjs.init("6cKvFXLfFf7eDWXKQ");
+    emailjs.send("service_vjkvo97","template_bo7u8n9",{
+      rsvp_name: this.rsvpForm.controls['submitterName'].value,
+      rsvp_lastname: this.rsvpForm.controls['submitterSurname'].value,
+      rsvp_phone: this.rsvpForm.controls['phone'].value,
+      rsvp_email: this.rsvpForm.controls['email'].value,
+      rsvp_comments: this.rsvpForm.controls['comments'].value,
+    });
+
+  }
 }
